997fd824fb5820f1753abc578a987b9979800de7
EunwooCho <jonnung@gmail.com>
2015-05-18 18:15:10 +0900

    알고스팟 문제 해결 가이드 (샘플 py 파일)

---
+++ Part1/Algospot/FESTIVAL/sample_problem_solving.py
@@ -0,0 +1,10 @@
+#-*- coding: utf8 -*-
+
+# 구현 로직이 들어갈 곳
+
+if __name__ == '__main__':
+    tc = int(raw_input())
+    for t in xrange(tc):
+        # 테스트 케이스마다 입력값을 처리할 부분
+        # 예) n, l = [int(x) for x in raw_input().split()]
+        pass
